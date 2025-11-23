67b24dfc65fdc4f998a35cef67c5a03d58c65fa0
Spider Supreme <132032656+Webwarrior20@users.noreply.github.com>
2025-11-23 15:41:45 -0700

    Add time selection and store to dashboard genres

--- pages/analytics/dashboard_genres.py
+++ pages/analytics/dashboard_genres.py
@@ -1,12 +1,14 @@
 import dash
-from dash import html
+from dash import html, dcc
 from layout.analytics_layout import analytics_layout
+from components.time_select_period import time_select_period
 
 dash.register_page(__name__, path="/analytics/genres", name="Your Top Genres - Analytics")
 
 layout = analytics_layout(
-        [
-
-        ],
+    [
+        time_select_period(),
+        dcc.Store(id="selected-time-period"),
+    ],
     "Top Genres"
-    )
+)
